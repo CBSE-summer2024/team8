3d419599eb4911973127d41014b8a9c3870ed2da
hadi sawalmeh <130775763+12114975@users.noreply.github.com>
2024-08-22 04:49:37 +0300

    create categories and sub categories but before final logic

--- firstapp/src/app/app.component.ts
+++ firstapp/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CategoryComponent} from './component/category/category.component';
+import {CategComponent} from './categ/categ.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CategoryComponent,CategComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+
 })
 export class AppComponent {
   title = 'firstapp';
---
+++ firstapp/src/app/categ/categ.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categ',
+  standalone: true,
+  imports: [],
+  templateUrl: './categ.component.html',
+  styleUrl: './categ.component.css'
+})
+export class CategComponent {
+
+}
