176e753d55e0d037f3af3f79198f731bb9cf0a4d
hadi sawalmeh <130775763+12114975@users.noreply.github.com>
2024-08-24 20:25:50 +0300

    data fetching

--- firstapp/src/app/app.component.ts
+++ firstapp/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {CategoryComponent} from './component/category/category.component';
-import {CategComponent} from './categ/categ.component'
+import {CategComponent} from './categ/categ.component';
+import {ProductComponent}from './product/product.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CategoryComponent,CategComponent],
+  imports: [RouterOutlet,CategoryComponent,CategComponent,ProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 
--- firstapp/src/app/categ/categ.component.ts
+++ firstapp/src/app/categ/categ.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductComponent } from '../product/product.component';
+
 
 @Component({
-  selector: 'app-categ',
-  standalone: true,
-  imports: [],
-  templateUrl: './categ.component.html',
-  styleUrl: './categ.component.css'
+  selector: 'app-categ',
+  standalone: true,
+  imports: [ProductComponent],
+  templateUrl: './categ.component.html',
+  styleUrl: './categ.component.css'
 })
 export class CategComponent {
+  selectedCategory: string = '';
+
+  constructor(private router: Router) {}
+
+
+  onCategorySelected(category: string) {
+    this.selectedCategory = category;
+    this.navigateToProduct(category); // Pass category to ProductComponent
+  }
+  navigateToProduct(category: string) {
+      this.router.navigate(['/product'], { queryParams: { category } });
+    }
 
 }
