b45b6a8aae4bf3571c45e2027b686faad35f9dd2
Raghad-Suwan <raghadsu979@gmail.com>
2024-09-01 00:34:37 +0300

    pull request

---
+++ my-project/src/vue-app/pages/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="home">
+      <h1>Welcome to mart.ps </h1> 
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "HomePage",
+  };
+  </script>
+  
+  <style scoped>
+  .home {
+    text-align: center;
+    margin-top: 50px;
+  }
+  
+  h1 {
+    color: #4CAF50;
+  }
+  
+  p {
+    font-size: 1.2rem;
+    color: #333;
+  }
+  </style>
